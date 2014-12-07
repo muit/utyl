f28ebe31601f6753bebc8e532eded260940bdf6d
muit <miguel_3c@hotmail.com>
2014-12-07 12:58:18 +0100

    0.3.3 - Array optimized each & GetNextFlip

--- utyl.js
+++ utyl.js
@@ -8,78 +8,96 @@
 Utyl = {
     author: "muitxer",
     repo: "http://github.com/muit/utyl",
-    version: "0.3.02"
-}
+    version: "0.3.3"
+};
 
 //*******************************
 // String helpers
 //*******************************
-String.prototype.startsWith = function(str){
+String.prototype.startsWith = function(str)
+{
     return this.slice(0, str.length) == str;
 };
 
-String.prototype.endsWith = function(suffix) {
+String.prototype.endsWith = function(suffix)
+{
     return this.indexOf(suffix, this.length - suffix.length) !== -1;
 };
 
-String.prototype.contains = function(str){
+String.prototype.contains = function(str)
+{
     return this.indexOf(str) != -1;
 };
 
 //*******************************
 // Hash helpers
 //*******************************
-Object.keys = function(object){
+Object.keys = function(object)
+{
     var ret=[],p;
     for(p in object) if(Object.prototype.hasOwnProperty.call(object,p)) ret.push(p);
     return ret;
-}
+};
 
-Object.key = function(object, o){
+Object.key = function(object, o)
+{
     var ret=[],p;
     for(p in object) if(object[p] == o) ret.push(p);
     return ret;
-}
+};
 
 //*******************************
 // Array Helpers
 //*******************************
-Array.prototype.first = function(){ return this[0]; }
-Array.prototype.last = function(){ return this[this.length-1]; }
-Array.prototype.remove = function(o){ 
+Array.prototype.first = function(){ return this[0]; };
+Array.prototype.last = function(){ return this[this.length-1];};
+Array.prototype.remove = function(o)
+{ 
     var i = this.indexOf(o);
     return (i >= 0)? this.splice(i, 1)[0] : undefined;
-}
-Array.prototype.removeByIndex = function(i){
+};
+Array.prototype.removeByIndex = function(i)
+{
     return (i >= 0)? this.splice(i, 1)[0] : undefined;
-}
+};
+Array.prototype.each = function(callback)
+{
+  if(typeof callback != "function") 
+    throw new Error("The callback provided is not a function.");
+  for (var i = 0, len = this.length; i < len; i++) {
+    callback(this[i]);
+  }
+};
+Array.prototype.getNextFlip = function()
+{
+  this.push(this.shift());
+  return this[0];
+};
 
 //*******************************
 // Player Array Helpers
 //*******************************
-Array.prototype.getByName = function(n){ 
+Array.prototype.getByName = function(n)
+{ 
     return this.filter(function(p){ return p.name == n; }).first();
-}
-
+};
 
 /*******************************
  * Trigger Class
  * @constructor 
  *******************************/
-Trigger = function(){}
-Trigger.prototype.get = function(){
-    return (!this.s)?this.s = true:false;
-}
-Trigger.prototype.reset = function(){
-    this.s = undefined;
-}
+Trigger = function(){};
+Trigger.prototype.get = function(){ return (!this.s)?this.s = true:false;};
+Trigger.prototype.reset = function(){ this.s = undefined;};
 
 /*******************************
  * Vector2 Class
  * @constructor 
  *******************************/
-Vector2 = function(x, y){
-    if(x instanceof Vector2){
+Vector2 = function(x, y)
+{
+    if(x instanceof Vector2)
+    {
         this.x = x.x; 
         this.y = x.y;
     }
@@ -87,22 +105,24 @@ Vector2 = function(x, y){
         this.x = x; 
         this.y = y;
     }
-}
+};
 Vector2.prototype.x = null;
 Vector2.prototype.y = null;
 Vector2.prototype.distance = function(v2){ return Vector2.distance(this, v2); };
 
 Vector2.distance = function(v1, v2){ 
     return Math.sqrt(Math.pow(v1.x + v2.x, 2) + Math.pow(v1.y + v2.y, 2)); 
-}
-Vector2.dot = function(v1, v2){ return v1.x * v2.x + v1.y * v2.y; }
+};
+Vector2.dot = function(v1, v2){ return v1.x * v2.x + v1.y * v2.y; };
 
 /*******************************
  * Vector3 Class
  * @constructor 
  *******************************/
-Vector3 = function(x, y, z){
-    if(x instanceof Vector3){
+Vector3 = function(x, y, z)
+{
+    if(x instanceof Vector3)
+    {
         this.x = x.x; 
         this.y = x.y; 
         this.z = x.z;
@@ -112,29 +132,32 @@ Vector3 = function(x, y, z){
         this.y = y; 
         this.z = z;
     }
-}
+};
 Vector3.prototype.x = null;
 Vector3.prototype.y = null;
 Vector3.prototype.z = null;
 Vector3.prototype.distance = function(v2){ return Vector3.distance(this, v2); };
 
-Vector3.distance = function(v1, v2){ 
+Vector3.distance = function(v1, v2)
+{ 
     return Math.sqrt(Math.pow(v1.x + v2.x, 2) + Math.pow(v1.y + v2.y, 2) + Math.pow(v1.z + v2.z, 2)); 
-}
-Vector3.dot = function(v1, v2){ return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+};
+Vector3.dot = function(v1, v2){ return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; };
 
 
 /*******************************
  * EventMap Class
  * @constructor 
  *******************************/
-EventMap = function(){}
+EventMap = function(){};
 EventMap.prototype.events = [];
 EventMap.prototype.callbacks = [];
 
-EventMap.prototype.createEvent = function(callback, delay){
+EventMap.prototype.createEvent = function(callback, delay)
+{
     var self = this;
-    var guid = setTimeout(function(){
+    var guid = setTimeout(function()
+    {
         self.removeEvent(guid);
         callback();
     }, delay);
@@ -142,49 +165,57 @@ EventMap.prototype.createEvent = function(callback, delay){
     this.events.push(guid);
     this.callbacks.push(callback);
     return this.events.length-1;
-}
+};
 
-EventMap.prototype.restartEvent = function(event, delay){
+EventMap.prototype.restartEvent = function(event, delay)
+{
     var index = this.events.indexOf(event);
-    if (index >= 0){
+    if (index >= 0)
+    {
         clearTimeout(this.events[index]);
         var self = this;
 
-        this.events[index] = setTimeout(function(){
+        this.events[index] = setTimeout(function()
+        {
             self.removeEvent(guid);
             self.callbacks[index]();
         }, delay);
     }
-}
+};
 
-EventMap.prototype.removeEvent = function(event){
+EventMap.prototype.removeEvent = function(event)
+{
     var index = this.events.indexOf(event);
     if (index >= 0) clearTimeout(this.events[index]);
     this.events.removeByIndex(index);
     this.callbacks.removeByIndex(index);
-}
+};
 
-EventMap.prototype.removeAllEvents = function(){
-    this.events.forEach(function(guid, index, array) {
+EventMap.prototype.removeAllEvents = function()
+{
+    this.events.forEach(function(guid, index, array)
+    {
         clearTimeout(guid);
     });
     this.events = [];
     this.callbacks = [];
-}
+};
 
 //*******************************
 // Reference Class
 //*******************************
-_ = function(variable){
+_ = function(variable)
+{
     if(variable instanceof Reference)
         return variable.value;
     var r = new Reference(variable);
     return r;
-}
+};
 /** @constructor **/
-Reference = function(variable){
+Reference = function(variable)
+{
     this._ = this.value = variable;
-}
+};
 
 
 //*******************************
@@ -192,71 +223,80 @@ Reference = function(variable){
 //
 // Comprovates if a class(or a subclass) is undefined and defines it.
 //*******************************
-ifNotDefined = function(classPath){
+ifNotDefined = function(classPath)
+{
     if(!classPath || classPath.length<=0) return undefined;
 
     classPath = classPath.split(".");
 
     var parent = this;
-    for(var i=0, len=classPath.length; i<len; i++){
+    for(var i=0, len=classPath.length; i<len; i++)
+    {
         if(parent[classPath[i]] == undefined)
             parent[classPath[i]] = function(){};
         parent = parent[classPath[i]];
     }
     return parent;
-}
+};
 
 
 //*******************************
 // Inherits method
 //*******************************
-Function.prototype.inherits = function(superClass){
+Function.prototype.inherits = function(superClass)
+{
     if(typeof superClass == "function")
         this.prototype = new superClass();
     else
         this.prototype = superClass;
     this.prototype.constructor = this;
-}
+};
 
 /*******************************
  * RGB Class
  * @constructor 
  *******************************/
-var RGB = function(r,g,b){
+var RGB = function(r,g,b)
+{
     this.set(r,g,b);
-}
-RGB.prototype.set = function(r,g,b){
-    if( (r >= 0 && r < 256) && (g >= 0 && g < 256) && (b >= 0 && b < 256)){
+};
+RGB.prototype.set = function(r,g,b)
+{
+    if( (r >= 0 && r < 256) && (g >= 0 && g < 256) && (b >= 0 && b < 256))
+    {
         this.red = r;
         this.green = g;
         this.blue = b;
         this.hex = (r*256*256+g*256+b).toString(16).toUpperCase();
     }
-}
+};
 /*******************************
  * RGBA Class
  * @constructor 
  * @extends RGB
  *******************************/
-var RGBA = function(r, g, b, a){
+var RGBA = function(r, g, b, a)
+{
     this.set(r,g,b);
     this.alpha = a;
-}
+};
 RGBA.inherits(RGB);
 
 
 //*******************************
 // Math randomRange
 //*******************************
-Math.randomRange = function(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;;
-}
+Math.randomRange = function(min, max)
+{
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 
 /*******************************
  * Timer Class
  * @constructor 
  *******************************/
-Timer = function(oninstance, fps, times){
+Timer = function(oninstance, fps, times)
+{
     var self = this;
     self.times = times;
     var speed = 1000/fps,
@@ -266,21 +306,21 @@ Timer = function(oninstance, fps, times){
 
     function instance()
     {
-            if(self.times && count++ >= self.times)
-                return;
-            now = new Date().getTime();
-            var diff = (now - last) - speed;
-            if(diff < 0) diff = 0;
-            last = now;            
-            if(!oninstance(diff/speed+1)){
-                setTimeout(instance, (speed-diff));
-            }
-    }
-    setTimeout(function(){
+        if(self.times && count++ >= self.times)
+            return;
+        now = new Date().getTime();
+        var diff = (now - last) - speed;
+        if(diff < 0) diff = 0;
+        last = now;            
+        if(!oninstance(diff/speed+1))
+            setTimeout(instance, (speed-diff));
+    };
+    setTimeout(function()
+    {
         last = new Date().getTime();
         instance();
     }, 0);
-}
+};
 Timer.prototype.close = function(){this.times = true};
 
 
